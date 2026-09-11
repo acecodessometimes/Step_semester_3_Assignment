e5845e2f19adcdd203f0e3f89f2026ea1964ed14
acecodes1301 <rb47996768@gmail.com>
2026-09-12 03:13:23 +0530

    Session 3: arrays assignment problems

---
+++ src/main/java/arrays/assigment_problems/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,27 @@
+public class FindMinimumInRotatedSortedArray {
+
+    static int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                // Minimum is somewhere to the right of mid
+                left = mid + 1;
+            } else {
+                // Minimum is at mid or somewhere to the left of mid
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findMin(new int[]{3, 4, 5, 1, 2}));
+        System.out.println(findMin(new int[]{4, 5, 6, 7, 0, 1, 2}));
+        System.out.println(findMin(new int[]{11, 13, 15, 17}));
+    }
+}
